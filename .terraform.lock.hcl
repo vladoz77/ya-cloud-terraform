f15d93c20bc524b503712906409dc333d59c96a5
Kovalev Vladislav <vladoz77@yandex.ru>
2024-12-09 10:56:27 +0300

    add devops-fabrika terraform demo

---
+++ .terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.134.0"
+  hashes = [
+    "h1:DwQDAu2qAQQiNHRs7xfVW6JVby+htS0J4k8QOlms7mI=",
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+# required for YC
+variable "token" {
+    description = "Token"
+    type = string
+    default = "y0_AgAAAAAHl17-AATuwQAAAAEbNL0XAAATJwcRdX5JRZY6rJhAZO48QfxO4w"
+}
+variable "cloud_id" {
+    description = "yandex cloud id"
+    type = string
+    default = "b1gfsoocn9at577iert4"
+}
+variable "folder_id" {
+    description = "Yandex folder_id"
+    type = string
+    default = "b1g4i80guj7e3cgo64om"
+}
+variable "zone" {
+    description = "Yandex Zone"
+    type = string
+    default = "ru-central1-a"
+}
